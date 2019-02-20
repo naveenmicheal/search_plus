1aad62cf38c73e67ea6da1ca4f1d8c5a89ed0a7a
Naveen Micheal <naveenmicheal@protonmail.com>
2019-02-20 22:02:31 +0530

    Added Popup Html and play around with content scripts

--- content.js
+++ content.js
@@ -1,1 +1,53 @@
-alert('GO >>>>>>>')
+console.log('Extension On Running......');
+
+// changing body color  
+var t1 = document.getElementsByTagName('body')
+t1[0].style.backgroundColor = 'black';
+
+var t2 = document.getElementById('hdtbSum')
+t2.style.backgroundColor = 'black';
+
+var t3 = document.getElementById('topabar')
+t3.style.backgroundColor = 'black';
+
+var t4 = document.getElementsByClassName('sfbg');
+t4[0].style.backgroundColor = 'black';
+
+// For Footer
+var t5 = document.getElementById('fbar');
+t5.style.backgroundColor = 'black';
+
+// Changing all Anchor tags 
+let s_heading = document.getElementsByTagName('a');
+for(var i = 0; i<s_heading.length; i++){
+	s_heading[i].style.color='white';
+}
+
+// Changing Block of search results
+let blocks = document.getElementsByClassName('rc');
+for (var i = 0 ; i < blocks.length; i++){
+	blocks[i].style.boxShadow = "10px 20px 30px #9b59b6"
+}
+let blt = document.getElementsByClassName('mn-dwn-arw');
+for (var i = 0 ; i < blt.length; i++){
+	blocks[i].style.boxShadow = "10px 20px 30px #9b59b6"
+}
+
+
+
+
+
+
+
+
+
+
+            
+            //+++++++++++Storage Action++++++++++++++++
+// chrome.storage.sync.set({'mykey':'myvalue'}, function (){
+// 	console.log('Value setted')
+// })
+// console.log('\n')
+// chrome.storage.sync.get(['mykey'], function(result){
+// 	console.log('Fetched Data ' + result.mykey)
+// })
